ba87e11c693c3b79b1b1aa92a91acb6406e34775
Nnamdi <emelunna@gmail.com>
2020-05-18 21:46:34 +0100

    corrected stickler error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,18 @@
 def bubble_sort(arr)
-  n = arr.length - 1
-    1.upto(n) do
-          n.times do |i|
-            if arr[i] > arr[i+1]
-            # arr[i], arr[i+1] = arr[i +1], arr[i]
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-            end
-         end
+n = arr.length - 1
+  1.upto(n) do
+    n.times do |i|
+      if arr[i] > arr[i + 1]
+        # arr[i], arr[i+1] = arr[i +1], arr[i]
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+      end
     end
-   return arr  
+  end
+arr  
 end
-# [4,3,78,2,0,2]
-arr = [5,1,4,2,8]
+# [4, 3, 78, 2, 0, 2]
+arr = [5, 1, 4, 2, 8]
 sorted_array = bubble_sort(arr)
 p sorted_array
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,7 +1,7 @@
 # arr = ['hi', 'hello', 'hey']
 arr = ['Originals', 'Hooked', 'Thinking', 'Fast and Slow', 'Nudge', 'Contagious']
 def bubble_sort_by(arr)
-  n = arr.length - 1
+n = arr.length - 1
   loop do
   swapped = false
     n.times do |i|
@@ -18,8 +18,6 @@ end
 bubble_sort_by(arr) do |left, right|
   left.length <=> right.length
 end
-# left.length <=> right.length
-# arr = ["hi","hello","hey"]
+# arr = ['hi', 'hello', 'hey']
 # bubble_sort_by(arr)
-
 p arr
