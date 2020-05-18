3680d1458430bc70457da20cf88db7e0963b8020
Nnamdi <emelunna@gmail.com>
2020-05-18 17:45:41 +0100

    project file set up

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+
+def bubble_sort(arr)
+    n = arr.length - 1
+    1.upto(n) do
+          n.times do |i|
+            if arr[i] > arr[i+1]
+            # arr[i], arr[i+1] = arr[i +1], arr[i]
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+            end
+         end
+    end
+   return arr  
+end
+# [4,3,78,2,0,2]
+arr = [5,1,4,2,8]
+sorted_array = bubble_sort(arr)
+p sorted_array
---
+++ bubble_sort_by.rb
@@ -0,0 +1,28 @@
+
+# arr = ["hi","hello","hey"]
+arr = ["Originals", "Hooked", "Thinking", "Fast and Slow", "Nudge", "Contagious"]
+
+def bubble_sort_by(arr)
+    n = arr.length - 1
+    loop do
+        swapped = false
+        n.times do |i|
+        result = yield arr[i], arr[i+1]
+        if result == 1
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            swapped = true  
+        end
+    end
+     break if !swapped
+   end
+   
+end
+
+bubble_sort_by(arr) do |left, right|
+    left.length <=> right.length
+end
+# left.length <=> right.length
+# arr = ["hi","hello","hey"]
+# bubble_sort_by(arr)
+
+p arr
