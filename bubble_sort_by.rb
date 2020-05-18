0d5dade60155bc1be0db38e00b8aa1a325f20e7c
Nnamdi <emelunna@gmail.com>
2020-05-18 21:08:07 +0100

    corrected stickler error

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,21 +1,18 @@
-
 # arr = ["hi","hello","hey"]
 arr = ["Originals", "Hooked", "Thinking", "Fast and Slow", "Nudge", "Contagious"]
-
 def bubble_sort_by(arr)
-    n = arr.length - 1
+  n = arr.length - 1
     loop do
-        swapped = false
+      swapped = false
         n.times do |i|
-        result = yield arr[i], arr[i+1]
-        if result == 1
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            swapped = true  
+          result = yield arr[i], arr[i + 1]
+            if result == 1
+              arr[i], arr[i + 1] = arr[i + 1], arr[i]
+              swapped = true  
+            end
         end
+      break if !swapped
     end
-     break if !swapped
-   end
-   
 end
 
 bubble_sort_by(arr) do |left, right|
